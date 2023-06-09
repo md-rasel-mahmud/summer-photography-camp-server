c89a7b1263527afb9063019d03347d320100c2b6
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2023-06-09 09:08:02 +0600

    Connect mongodb and added classes get method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 4000;
 
@@ -8,8 +9,44 @@ const port = process.env.PORT || 4000;
 app.use(cors())
 app.use(express.json())
 
+// mongodb connection 
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const classes = client.db('spc').collection('classes')
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // classes related apis 
+    app.get('/classes', async (req, res) => {
+        const result = await classes.find().toArray()
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', async (req, res) => {
-    console.log('server is live');
+    res.send('server is live')
 })
 
 app.listen(port, () => {
