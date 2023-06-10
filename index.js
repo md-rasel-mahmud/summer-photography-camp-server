4b4c9c74ba55cc46c978e582b5a7fc2e0fe15c87
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2023-06-10 12:05:29 +0600

    create selected class api

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express()
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 4000;
 
-// middleware 
-app.use(cors())
-app.use(express.json())
+// middleware
+app.use(cors());
+app.use(express.json());
 
-// mongodb connection 
+// mongodb connection
 const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,32 +18,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-const classes = client.db('spc').collection('classes')
-const users = client.db('spc').collection('users') 
-const selectedClasses = client.db('spc').collection('selectedClasses') 
+const classes = client.db("spc").collection("classes");
+const users = client.db("spc").collection("users");
+const selectedClasses = client.db("spc").collection("selectedClasses");
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    // classes related apis 
-    app.get('/classes', async (req, res) => {
-        const result = await classes.find().toArray()
-        res.send(result)
-    })
+    // classes related apis
+    app.get("/classes", async (req, res) => {
+      const result = await classes.find().toArray();
+      res.send(result);
+    });
 
-    // users related apis 
-    app.post('/selected-classes', async (req, res) => {
+    // users related apis
+    app.post("/selected-classes", async (req, res) => {
       const classes = req.body;
       const result = await selectedClasses.insertOne(classes);
-      res.send(result)
-    })
-
-    app.post('/user', async(req, res) => {
+      res.send(result);
+    });
+    app.get("/selected-classes", async (req, res) => {
+      const email = req.query.email;
+      const result = await selectedClasses.find({ email }).toArray();
+      res.send(result);
+    });
+    app.post("/user", async (req, res) => {
       const user = req.body;
       console.log(user);
       const existingUser = await users.findOne({ email: user?.email });
@@ -55,11 +59,13 @@ async function run() {
       const result = await users.insertOne(user);
 
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -67,11 +73,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', async (req, res) => {
-    res.send('server is live')
-})
+app.get("/", async (req, res) => {
+  res.send("server is live");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
